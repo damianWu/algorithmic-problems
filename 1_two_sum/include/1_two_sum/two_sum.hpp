459106b79748fcf53f0c36d57fad5d74386bc713
Damian Wu <damian.wolonkiewicz@gmail.com>
2020-09-25 17:57:11 +0200

    Added problem 1 description

--- 1_two_sum/include/1_two_sum/two_sum.hpp
+++ 1_two_sum/include/1_two_sum/two_sum.hpp
@@ -2,5 +2,18 @@
 #define TWO_SUM
 
 
+// Given an array of integers, return INDICES of the two numbers
+// such that they add up to a specific target.
+// You may assume that each input would have EXACTLY one solution,
+// and you may not use the SAME element twice.
+
+// Example 1:
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+// Output: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+
+
+
 
 #endif // TWO_SUM
