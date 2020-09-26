1b15390ebed1e1859108ec07c19cc73b61821a9f
Damian Wolonkiewicz <damian.wolonkiewicz@gmail.com>
2020-09-26 11:55:08 +0200

    New test added, checking if EmptyVectorException was thrown

--- tests/test_two_sum.cpp
+++ tests/test_two_sum.cpp
@@ -3,30 +3,44 @@
 #include "1_two_sum/two_sum.hpp"
 
 TEST(TestTwoSum, ShouldReturnNegativeIndexValue) {
-    // given
-    std::vector<int> v{2, 7, 11, 15};
-    int target{7};
+  // given
+  std::vector<int> v{2, 7, 11, 15};
+  int target{7};
 
-    // when
-    auto [i, j]{two_sum_on2(v, target)};
+  // when
+  auto [i, j]{two_sum_on2(v, target)};
 
-    // then
-    ASSERT_EQ(-1, i);
-    ASSERT_EQ(-1, j);
+  // then
+  ASSERT_EQ(-1, i);
+  ASSERT_EQ(-1, j);
 }
-
 
 TEST(TestTwoSum, ShouldReturnExpectedIndexValues) {
-    // given
-    std::vector<int> v{2, 5, 11, 15};
-    int target{7};
+  // given
+  std::vector<int> v{2, 5, 11, 15};
+  int target{7};
 
-    // when
-    auto [i, j]{two_sum_on2(v, target)};
+  // when
+  auto [i, j]{two_sum_on2(v, target)};
 
-    // then
-    ASSERT_EQ(0, i);
-    ASSERT_EQ(1, j);
+  // then
+  ASSERT_EQ(0, i);
+  ASSERT_EQ(1, j);
 }
 
+TEST(TestTwoSum, ShouldThrowExceptionWhenVectorEmpty) {
+  // given
+  std::vector<int> v;
+  int target{7};
 
+  try {
+    // when
+    two_sum_on2(v, target);
+    FAIL();
+  } catch (const EmptyVectorException &err) {
+    // then
+    ASSERT_STREQ(
+        "const std::pair<int, int> two_sum_on2(...). Input vector was empty.",
+        err.what());
+  }
+}
