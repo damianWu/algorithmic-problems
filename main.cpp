8a9bfc96e2dec79a972864148a8845d69fbe42f3
Damian Wolonkiewicz <damian.wolonkiewicz@gmail.com>
2020-09-30 00:55:54 +0200

    Prepared main function for optimal solution

--- main.cpp
+++ main.cpp
@@ -3,9 +3,9 @@
 #include "1_two_sum/two_sum.hpp"
 
 int main() {
-    std::vector<int> v{2, 7, 11, 15};
-    int target{7};
-    auto [i, j]{two_sum_on2(v, target)};
+    std::vector<int> v{2, 17, 11, 7, 15};
+    int target{9};
+    auto[i, j]{two_sum_optimal(v, target)};
     std::cout << "i: " << i << " j: " << j << "\n";
     return 0;
 }
