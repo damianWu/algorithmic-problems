041b43e691a137ce183654c4f92ad748e1c8deac
Damian Wolonkiewicz <damian.wolonkiewicz@gmail.com>
2020-09-29 21:20:24 +0200

    Fixed serious bug in algorithm

--- 1_two_sum/source/two_sum.cpp
+++ 1_two_sum/source/two_sum.cpp
@@ -9,8 +9,10 @@ const std::pair<int, int> two_sum_on2(const std::vector<int> &numbers,
 
     for (unsigned long long i = 0; i < numbers.size(); ++i) {
         for (unsigned long long j = 0; j < numbers.size(); ++j) {
-            if (numbers.at(i) + numbers.at(j) == target) {
-                return std::make_pair(i, j);
+            if (i != j) {
+                if (numbers.at(i) + numbers.at(j) == target) {
+                    return std::make_pair(i, j);
+                }
             }
         }
     }
