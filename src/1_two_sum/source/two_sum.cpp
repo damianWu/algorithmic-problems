c5805d8b052150efa34cda9b148e63c016107e1a
Damian Wolonkiewicz <damian.wolonkiewicz@gmail.com>
2020-09-30 01:45:59 +0200

    Improved functions implementation and replace for loop with while

--- src/1_two_sum/source/two_sum.cpp
+++ src/1_two_sum/source/two_sum.cpp
@@ -25,29 +25,24 @@ std::pair<int, int> two_sum_naive(const std::vector<int> &numbers,
 
 std::pair<int, int> two_sum_optimal(const std::vector<int> &numbers,
                                     int target) {
-    // number, index
-    std::vector<std::pair<int, int>> numbers_indexes;
-    // Reserve space in vector
-    numbers_indexes.reserve(numbers.size());
-    buildListOfPairsNumberIndex(numbers, numbers_indexes);
-    std::sort(numbers_indexes.begin(), numbers_indexes.end());
-    return findIndices(numbers_indexes, target);
+    auto number_index{buildListOfPairsNumberIndex(numbers)};
+    std::sort(number_index.begin(), number_index.end());
+    return findIndices(number_index, target);
 }
 
 std::pair<int, int> findIndices(
-        const std::vector<std::pair<int, int>> &numbers_indexes,
+        const std::vector<std::pair<int, int>> &number_index,
         int target) {
-
     unsigned long long first_index{0};
-    unsigned long long second_index{numbers_indexes.size() - 1};
+    unsigned long long second_index{number_index.size() - 1};
 
-    for (unsigned long long i = 0; i < numbers_indexes.size(); ++i) {
-        auto sum{numbers_indexes.at(first_index).first +
-                 numbers_indexes.at(second_index).first};
+    while (first_index < second_index) {
+        auto sum{number_index.at(first_index).first +
+                 number_index.at(second_index).first};
         if (sum == target) {
             return {
-                    numbers_indexes.at(first_index).second,
-                    numbers_indexes.at(second_index).second
+                    number_index.at(first_index).second,
+                    number_index.at(second_index).second
             };
         } else if (sum > target) {
             --second_index;
@@ -62,9 +57,14 @@ std::pair<int, int> findIndices(
             " but no one was found."};
 }
 
-void buildListOfPairsNumberIndex(const std::vector<int> &numbers,
-                                 std::vector<std::pair<int, int>> &numbers_indexes) {
+std::vector<std::pair<int, int>>
+buildListOfPairsNumberIndex(const std::vector<int> &numbers) {
+    // number, index
+    std::vector<std::pair<int, int>> number_index;
+    // Reserve space in vector
+    number_index.reserve(numbers.size());
     for (unsigned long long i = 0; i < numbers.size(); ++i) {
-        numbers_indexes.emplace_back(numbers.at(i), i);
+        number_index.emplace_back(numbers.at(i), i);
     }
+    return number_index;
 }
