cb3402d6ff7134bc1efaa28e995bf7770b6ecf26
Damian Wolonkiewicz <damian.wolonkiewicz@gmail.com>
2020-09-30 13:50:50 +0200

    Added tests for optimal solution

--- tests/test_two_sum.cpp
+++ tests/test_two_sum.cpp
@@ -103,3 +103,88 @@ TEST_F(TwoSumNaiveTest,
                      e.what());
     }
 }
+
+
+/**
+ * Optimal
+ */
+
+TEST_F(TwoSumOptimalTest, ShouldThrowExceptionWhenTargetNotReached) {
+    // Given
+    std::vector<int> v{2, 7, 11, 15};
+
+    try {
+        // When
+        two_sum_optimal(v, target);
+        FAIL();
+    } catch (const TargetNotFoundException &e) {
+        // Then
+        ASSERT_STREQ("std::pair<int, int> findIndices(...) "
+                     "Task conditions guarantee existing of target,"
+                     " but no one was found.",
+                     e.what());
+    }
+}
+
+TEST_F(TwoSumOptimalTest, ShouldReturnExpectedIndexValues) {
+    // Given
+    std::vector<int> v{2, 5, 11, 15};
+    const unsigned long long i_index_expected{0};
+    const unsigned long long j_index_expected{1};
+
+    // When
+    auto[i, j]{two_sum_optimal(v, target)};
+
+    // Then
+    ASSERT_EQ(i_index_expected, i);
+    ASSERT_EQ(j_index_expected, j);
+}
+
+TEST_F(TwoSumOptimalTest, ShouldThrowExceptionWhenInputVectorIsEmpty) {
+    // Given
+    std::vector<int> v;
+
+    try {
+        // When
+        two_sum_optimal(v, target);
+        FAIL();
+    } catch (const EmptyVectorException &err) {
+        // Then
+        ASSERT_STREQ(
+                "std::pair<int, int> two_sum_optimal(...) "
+                "Input vector was empty.",
+                err.what());
+    }
+}
+
+TEST_F(TwoSumOptimalTest, ShouldReturnCorrectIndexWhenNegativeNumbersInVector) {
+    // Given
+    std::vector<int> v{-1, -24, -673, 9, 346, -7543, -2, 113};
+    const unsigned long long i_expected_index{6};
+    const unsigned long long j_expected_index{3};
+
+    // When
+    auto[i, j]{two_sum_optimal(v, target)};
+
+    // Then
+    ASSERT_EQ(i, i_expected_index);
+    ASSERT_EQ(j, j_expected_index);
+}
+
+TEST_F(TwoSumOptimalTest,
+       ShouldThrowExceptionWhenTargetExistButDoNotSatisfyProblemBoundaries) {
+    // Given
+    std::vector<int> v{2, 3, 11, 15};
+    target = 6;
+    try {
+        // When
+        two_sum_optimal(v, target);
+        FAIL();
+    } catch (const TargetNotFoundException &e) {
+        // Then
+        ASSERT_STREQ("std::pair<int, int> findIndices(...) "
+                     "Task conditions guarantee existing of target,"
+                     " but no one was found.",
+                     e.what());
+    }
+}
