2aa1cede7b7fe836899bfe726a0b18f644c44f63
Damian Wolonkiewicz <damian.wolonkiewicz@gmail.com>
2020-09-30 22:29:15 +0200

    Added function definition and format file

--- src/unique_list/include/unique_list/unique_list.hpp
+++ src/unique_list/include/unique_list/unique_list.hpp
@@ -1,8 +1,10 @@
-#ifndef ALGORITHMS_DATA_STRUCTURES_UNIQUE_LIST_H
-#define ALGORITHMS_DATA_STRUCTURES_UNIQUE_LIST_H
+#ifndef ALGORITHMS_DATA_STRUCTURES_UNIQUE_LIST_HPP
+#define ALGORITHMS_DATA_STRUCTURES_UNIQUE_LIST_HPP
 
+#include <set>
 #include <vector>
 
 std::vector<int> getUnique(const std::vector<int> &);
+std::vector<int> getUniqueSet(const std::vector<int> &);
 
-#endif //ALGORITHMS_DATA_STRUCTURES_UNIQUE_LIST_H
+#endif //ALGORITHMS_DATA_STRUCTURES_UNIQUE_LIST_HPP
