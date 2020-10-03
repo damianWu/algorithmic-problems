ab6147a518bf41f804474fe0f235766f6a3ce2ea
Damian Wolonkiewicz <damian.wolonkiewicz@gmail.com>
2020-10-03 14:06:53 +0200

    Added whitespace

--- src/unique_list/include/unique_list/unique_list.hpp
+++ src/unique_list/include/unique_list/unique_list.hpp
@@ -5,6 +5,7 @@
 #include <vector>
 
 std::vector<int> getUnique(const std::vector<int> &);
+
 std::vector<int> getUniqueSet(const std::vector<int> &);
 
 #endif //ALGORITHMS_DATA_STRUCTURES_UNIQUE_LIST_HPP
