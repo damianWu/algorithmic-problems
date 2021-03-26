c7a6bc3db1c4ac6595f1f0587de71b033dc28c6f
Damian Wolonkiewicz <damian.wolonkiewicz@gmail.com>
2021-03-26 21:51:38 +0100

    Main changes

--- main.cpp
+++ main.cpp
@@ -3,8 +3,8 @@
 #include "1_two_sum/two_sum.hpp"
 
 int main() {
-    std::vector<int> v{2, 7, 11, 15};
-    int target{7};
+    std::vector<int> v{2, 3, 11, 15};
+    int target{6};
     auto [i, j]{two_sum_on2(v, target)};
     std::cout << "i: " << i << " j: " << j << "\n";
     return 0;
